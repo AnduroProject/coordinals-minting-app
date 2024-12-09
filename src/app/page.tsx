e5ca39457b16ee8054d071ca5be9850e584ff4fc
neel <neelu@bsetxt.com>
2024-12-09 13:14:24 +0530

    popup updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import Link from "next/link";
 import Header from "@/components/layout/header";
 import Image from "next/image";
 import Options from "@/components/section/options";
@@ -93,6 +94,62 @@ const Create = () => {
           </ButtonLg>
         </div>
       </div>
+
+      <div className="fixed top-0 left-0 w-full h-full bg-overlayRgb flex justify-center items-center z-50 hidden">
+       <div className="bg-white rounded-lg max-w-2xl w-full relative">
+        <div className="grid grid-cols-12">
+         <div className="col-span-4">
+          <div className="p-5 bg-neutral100 rounded-l-lg">
+           <Link href={"/"}>
+              <Image src={"/Logo.svg"} alt="coordinals" width={160} height={40} />
+            </Link>
+           <h4 className="my-3">Connect your network</h4>
+           <p className="text-sm mb-3">Connecting your network is like "logging in to Web3. Select your network from the options to get started.</p>
+           <Link className="text-sm text-neutral700" href={"/"}>I don't have a network</Link>
+          </div> 
+         </div>
+         <div className="col-span-8">
+          <div className="border-b border-gray-400 flex flex-row justify-between items-center px-2">
+           <h5 className>Available Networks (2)</h5>
+           <button className="bg-transparent border-none text-2xl">&times;</button>
+          </div>
+          <div className="grid grid-cols-12 gap-2 mt-4 px-2">
+           <div className="col-span-6">
+            <div className="border border-gray-400 p-2 rounded-lg flex flex-row items-center">
+             <div className="border border-gray-400 p-1.5 rounded-lg">
+              <Image
+                src={"/cbtc.svg"}
+                alt="background"
+                width={20}
+                height={20}
+                sizes="100%"
+                className="object-cover w-5 h-5"
+              />
+             </div>
+             <p className="pl-2 text-base">cBTC Network</p>
+            </div>
+           </div>
+           <div className="col-span-6">
+            <div className="border border-gray-400 p-2 rounded-lg flex flex-row items-center">
+             <div className="border border-gray-400 p-1.5 rounded-lg">
+              <Image
+                src={"/alys.svg"}
+                alt="background"
+                width={20}
+                height={20}
+                sizes="100%"
+                className="object-cover w-5 h-5 rounded-full"
+              />
+             </div>
+             <p className="pl-2 text-base">aBTC Network</p>
+            </div>
+           </div>
+          </div>
+         </div> 
+        </div>
+      </div>
+     </div>
+
     </Layout>
   );
 };
