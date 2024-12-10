b99e78c293c0acc7721097098ae13b82ed17cf66
hemalathabsetec <hemalatha@bsetec.com>
2024-12-10 11:26:44 +0530

    meta data upload changes

--- src/app/api/metaData/route.ts
+++ src/app/api/metaData/route.ts
@@ -1,4 +1,7 @@
 import { NextResponse } from "next/server";
+var AWS = require('aws-sdk');
+
+
 import path from "path";
 const fs = require("fs")
 
@@ -6,20 +9,28 @@ const fs = require("fs")
 export async function POST(req: Request) {
   try {
     const { jsonData,tokenId } = await req.json();
-console.log("====json data",jsonData)
-console.log("====json tokenId",tokenId)
-
-    //folder and filename 
-    const folderPath = path.join(process.cwd(), "data");
-    const filePath = path.join(folderPath, tokenId +".json");
-
-    
-    if (!fs.existsSync(folderPath)) {
-      fs.mkdirSync(folderPath, { recursive: true });
+    const payload = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: `nft/${tokenId}.json`,
+      Body: JSON.stringify(jsonData),
+      ContentType: 'application/json',
     }
-
-    fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
-    return NextResponse.json({ message: "JSON file saved successfully" }, { status: 200 });
+    AWS.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey:  process.env.AWS_SECRET_ACCESS_KEY,
+      region:  process.env.AWS_REGION,
+    })  
+    const s3 = new AWS.S3()
+    // convert the callback-based function to a promise-based function provided by aws-sdk
+    const uploadPromise = s3.upload(payload).promise()
+    // wait for the promise to resolve
+    const data = await uploadPromise  
+    return NextResponse.json({
+      message: 'File uploaded successfully',
+      data: {
+          fileUrl: data.Location,
+      }
+    }, { status: 200 });
   } catch (error) {
     console.error("Error writing JSON file:", error);
     return NextResponse.json({ message: "Failed to save JSON file" }, { status: 500 });
